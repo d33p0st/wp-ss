f5fcc40902cc17cc03ad78d7c2b4b5e66bc533a1
d33p0st <d33p0st@gmail.com>
2024-10-19 09:25:31 +0530

    Updated Schedule

--- schedule.bat
+++ schedule.bat
@@ -1,11 +1,17 @@
 @echo off
 
+REM Check if the command with arguments is provided
+if "%*"=="" (
+    echo Error: No command provided.
+    echo Usage: schedule.bat "command with arguments"
+    exit /b 1
+)
+
 REM Define the task name and the command to run (in the background)
 set TASK_NAME=WPSS
-set COMMAND=wpss
 
 REM Create a scheduled task to run the command at every startup
-schtasks /create /tn %TASK_NAME% /tr %COMMAND% /sc onlogon /rl highest /f
+schtasks /create /tn %TASK_NAME% /tr "%*" /sc onlogon /rl highest /f
 
 REM Confirm task creation
 if %errorlevel%==0 (
